05c6d5fa86ddf639cb700545ff7fde0407d03a16
ZakhTania <tatizarova@gmail.com>
2024-02-06 18:09:26 +0300

    add level radiotoolbar

--- src/pages/SelectLevelPage/SelectLevelPage.jsx
+++ src/pages/SelectLevelPage/SelectLevelPage.jsx
@@ -2,13 +2,19 @@ import { Link } from "react-router-dom";
 import styles from "./SelectLevelPage.module.css";
 import { Checkbox } from "../../components/Checkbox/Checkbox";
 import { Button } from "../../components/Button/Button";
+import { useState } from "react";
 
 export function SelectLevelPage() {
+  const levelList = [1, 2, 3];
+  const [levelLink, setLevelLink] = useState("3");
+  function handleChangeLevel(level) {
+    setLevelLink(level * 3);
+  }
   return (
     <div className={styles.container}>
       <div className={styles.modal}>
         <h1 className={styles.title}>Выбери сложность</h1>
-        <ul className={styles.levels}>
+        {/* <ul className={styles.levels}>
           <li className={styles.level}>
             <Link className={styles.levelLink} to="/game/3">
               1
@@ -24,9 +30,27 @@ export function SelectLevelPage() {
               3
             </Link>
           </li>
-        </ul>
+        </ul> */}
+        <div className={styles.levels}>
+          {levelList.map(level => {
+            return (
+              <label htmlFor={`level${level}`} className={styles.levelLink} key={`level${level}${level}`}>
+                <input
+                  name="levelchoose"
+                  className={styles.level}
+                  type="radio"
+                  id={`level${level}`}
+                  onChange={() => handleChangeLevel(level)}
+                />
+                {level}
+              </label>
+            );
+          })}
+        </div>
         <Checkbox>3 попытки</Checkbox>
-        <Button>Старт</Button>
+        <Link to={`/game/${levelLink}`}>
+          <Button>Старт</Button>
+        </Link>
       </div>
     </div>
   );

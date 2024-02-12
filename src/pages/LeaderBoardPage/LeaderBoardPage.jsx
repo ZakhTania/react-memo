6923479271931050ca876eab202a82175666725a
ZakhTania <tatizarova@gmail.com>
2024-02-12 15:57:18 +0300

    getting data for LeaderBoard from API

--- src/pages/LeaderBoardPage/LeaderBoardPage.jsx
+++ src/pages/LeaderBoardPage/LeaderBoardPage.jsx
@@ -1,8 +1,21 @@
 import { Link } from "react-router-dom";
 import { Button } from "../../components/Button/Button";
 import styles from "./LeaderBoardPage.module.css";
+import { getLeadersList } from "../../API";
+import { useEffect, useState } from "react";
 
 export function LeaderBoard() {
+  const [leadersList, setLeadersList] = useState([]);
+  useEffect(() => {
+    getLeadersList()
+      .then(data => {
+        setLeadersList(data.leaders);
+      })
+      .catch(error => {
+        console.log(error.message);
+      });
+  }, []);
+
   return (
     <div className={styles.container}>
       <div className={styles.header}>
@@ -14,8 +27,18 @@ export function LeaderBoard() {
       <div className={styles.leaderListHeader}>
         <div className={styles.leaderListHeaderEl}>Позиция</div>
         <div className={styles.leaderListHeaderEl}>Пользователь</div>
-        <div className={styles.leaderListHeaderEl}></div>
         <div className={styles.leaderListHeaderEl}>Время</div>
+      </div>
+      <div>
+        {leadersList.map(leader => {
+          return (
+            <div className={styles.leaderList} key={leader.id}>
+              <div className={styles.leaderListEl}>{leader.id}</div>
+              <div className={styles.leaderListEl}>{leader.name}</div>
+              <div className={styles.leaderListEl}>{leader.time}</div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
